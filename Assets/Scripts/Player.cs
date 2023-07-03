91258b5e4fe53d443fb1389d4ee24f890f801a3a
tristanhbigfatbrainstorm <92125539+TristanChadHafkenscheid@users.noreply.github.com>
2023-07-03 13:37:32 -0400

    added movement to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f;
+
     void Start()
     {
         transform.position = new Vector3 (0, 0, 0);
@@ -11,6 +14,27 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(_speed * Time.deltaTime * direction);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
