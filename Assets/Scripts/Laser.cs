603087ae89ad5164ff98bcffb691c1e91ab8912b
tristanhbigfatbrainstorm <92125539+TristanChadHafkenscheid@users.noreply.github.com>
2023-07-04 23:48:14 -0400

    added shooting to player and made an enemy prefab

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime * Vector3.up);
+
+        if (transform.position.y > 8.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,25 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = 0.1f;
 
     void Start()
     {
-        transform.position = new Vector3 (0, 0, 0);
+        transform.position = new Vector3(0, 0, 0);
     }
 
     void Update()
     {
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            ShootLaser();
+        }
     }
 
     private void CalculateMovement()
@@ -36,5 +46,11 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
+    }
+
+    private void ShootLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
     }
 }
