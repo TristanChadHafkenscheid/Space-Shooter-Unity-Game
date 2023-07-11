eebee8276cd92be00e148055e8dfe1f7fb6f750d
Tristan Hafkenscheid <92125539+TristanChadHafkenscheid@users.noreply.github.com>
2023-07-11 17:18:03 -0400

    added asteroid that is shootable and explodes
    asteroid is prefab that can be shot
    explosion prefab is instantiated when laser collides with it

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotateSpeed = 20.0f;
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Laser"))
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
