c55d510b5184914842b583ded0e752424d173cf5
Tristan Hafkenscheid <92125539+TristanChadHafkenscheid@users.noreply.github.com>
2023-07-28 15:36:14 -0400

    added escape key to quit game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,11 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene(1); //current game scene
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     public void GameOver()
