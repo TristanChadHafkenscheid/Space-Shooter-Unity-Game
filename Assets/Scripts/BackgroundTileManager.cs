26e1e7736748790707cc84c829cbb14af91b337a
Tristan Hafkenscheid <92125539+TristanChadHafkenscheid@users.noreply.github.com>
2023-09-05 17:13:47 -0400

    all sides work for background manager

--- Assets/Scripts/BackgroundTileManager.cs
+++ Assets/Scripts/BackgroundTileManager.cs
@@ -6,37 +6,85 @@ public class BackgroundTileManager : MonoBehaviour
 {
     [SerializeField] private Transform[] _backgroundArray;
     [SerializeField] float _tileDistance = 20.48f;
+    [SerializeField] float _safeDistanceX  = 6;
+    [SerializeField] float _safeDistanceY = 2.5f;
+
     private int _farthestTile = 0;
     private int _currentTile = 0;
     private Transform _playerTransform;
-    [SerializeField] float _safeDistance;
-    private bool _isFarRightTileSet = false;
+
+    private bool _isFarTileSet = false;
 
     void Start()
     {
         _playerTransform = GameObject.FindWithTag("Player").transform;
-        FindCurrentTile();
+        _currentTile = FindCurrentTile();
     }
 
     void Update()
     {
-        Debug.Log("Current tile pos - player pos: " + (_backgroundArray[_currentTile].position.x - _playerTransform.position.x));
-        Debug.Log("Current tile pos - safe distance: " + (_safeDistance));
+        Debug.Log("Current tile - player: " + (_backgroundArray[_currentTile].position - _playerTransform.position));
 
-        _currentTile = FindCurrentTile();
+        //_currentTile = FindCurrentTile();
 
+        //moving left
         if (_backgroundArray[_currentTile].position.x - _playerTransform.position.x >=
-            _safeDistance && _isFarRightTileSet == false)
+            _safeDistanceX && _isFarTileSet == false)
         {
             _currentTile = FindCurrentTile();
             _farthestTile = FindFarthestTile();
 
-            SetFarRightTile();
+            SetFarTile(Vector3.left);
         }
         else if (_backgroundArray[_currentTile].position.x - _playerTransform.position.x <
-            _safeDistance)
+            _safeDistanceX)
         {
-            _isFarRightTileSet = false;
+            _isFarTileSet = false;
+        }
+
+        //moving right
+        if (_backgroundArray[_currentTile].position.x - _playerTransform.position.x <=
+            -_safeDistanceX && _isFarTileSet == false)
+        {
+            _currentTile = FindCurrentTile();
+            _farthestTile = FindFarthestTile();
+
+            SetFarTile(Vector3.right);
+        }
+        else if (_backgroundArray[_currentTile].position.x - _playerTransform.position.x >
+            -_safeDistanceX)
+        {
+            _isFarTileSet = false;
+        }
+
+        //moving up
+        if (_backgroundArray[_currentTile].position.y - _playerTransform.position.y <=
+            -_safeDistanceY && _isFarTileSet == false)
+        {
+            _currentTile = FindCurrentTile();
+            _farthestTile = FindFarthestTile();
+
+            SetFarTile(Vector3.up);
+        }
+        else if (_backgroundArray[_currentTile].position.y - _playerTransform.position.y >
+            -_safeDistanceY)
+        {
+            _isFarTileSet = false;
+        }
+
+        //moving down
+        if (_backgroundArray[_currentTile].position.y - _playerTransform.position.y >=
+            _safeDistanceY && _isFarTileSet == false)
+        {
+            _currentTile = FindCurrentTile();
+            _farthestTile = FindFarthestTile();
+
+            SetFarTile(Vector3.down);
+        }
+        else if (_backgroundArray[_currentTile].position.y - _playerTransform.position.y <
+            _safeDistanceY)
+        {
+            _isFarTileSet = false;
         }
     }
 
@@ -74,10 +122,10 @@ public class BackgroundTileManager : MonoBehaviour
         return _currentTile;
     }
 
-    private void SetFarRightTile()
+    private void SetFarTile(Vector3 tileDirection)
     {
         _backgroundArray[_farthestTile].position = _backgroundArray[_currentTile].position
-            + Vector3.left * _tileDistance;
-        _isFarRightTileSet = true;
+            + tileDirection * _tileDistance;
+        _isFarTileSet = true;
     }
 }
